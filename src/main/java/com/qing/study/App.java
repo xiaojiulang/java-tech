ff29e109d3363386135556fe03c85fd72e72862a
xiaojiulang <a74186438@163.com>
2020-05-27 10:50:08 +0800

    initial

---
+++ src/main/java/com/qing/study/App.java
@@ -0,0 +1,13 @@
+package com.qing.study;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
